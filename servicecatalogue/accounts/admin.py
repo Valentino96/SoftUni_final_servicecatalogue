839f8a81b9973eadb652ba330f9e3118c7623881
Valentino96 <valentino_hristov@abv.bg>
2022-12-14 14:21:07 +0200

    Changes in admin files

--- servicecatalogue/accounts/admin.py
+++ servicecatalogue/accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin, get_user_model
 
+from servicecatalogue.accounts.forms import UserEditForm, UserCreateForm
 from servicecatalogue.accounts.models import CustomerProfile, ProviderProfile
 
 UserModel = get_user_model()
@@ -8,15 +9,27 @@ UserModel = get_user_model()
 
 @admin.register(UserModel)
 class UserAdmin(auth_admin.UserAdmin):
-    list_display = ('email', )
-    ordering = ('-email',)
-    list_filter = ('email', )
+    form = UserEditForm
+    add_form = UserCreateForm
+    list_display = ('email', 'is_staff')
+    list_filter = ('is_staff',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_customer', 'is_provider',
-         'is_superuser', 'groups', 'user_permissions')}),
-        ('Dates', {'fields': ('last_login', )})
+        ('Personal info', {'fields': ('is_customer', 'is_provider')}),
+        ('Permissions', {'fields': ('is_staff',)}),
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+    def get_form(self, request, obj=None, **kwargs):
+        return super().get_form(request, obj, **kwargs)
 
 
 @admin.register(CustomerProfile)
--- servicecatalogue/accounts/forms.py
+++ servicecatalogue/accounts/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import forms as auth_forms, get_user_model
 from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
 from servicecatalogue.accounts.models import CustomerProfile, ProviderProfile
 
@@ -68,3 +70,33 @@ class ProviderEditForm(auth_forms.UserChangeForm):
     class Meta:
         model = ProviderProfile
         fields = ('first_name', 'last_name', 'job',  'address', 'phone_number')
+
+
+class UserEditForm(auth_forms.UserChangeForm):
+    password = ReadOnlyPasswordHashField()
+    class Meta:
+        model = UserModel
+        fields = ('email', 'password', 'is_provider', 'is_customer', 'is_staff')
+
+
+class UserCreateForm(auth_forms.UserCreationForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = UserModel
+        fields = ('email', )
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
